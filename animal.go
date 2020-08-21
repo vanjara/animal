15aad20d0a1fe322fa2d72778926a422cb709555
vanjara <vanjara@outlook.com>
2020-08-21 15:54:26 -0700

    adding more test cases

--- animal.go
+++ animal.go
@@ -9,3 +9,12 @@ func New() game {
 		Running: true,
 	}
 }
+
+func AskUserYesOrNo(question string) string {
+	//Extend test to not pass by just returning the question
+	//enter muliple test cases
+	if question == "Is it a horse" {
+		return "yes"
+	}
+	return "no"
+}
