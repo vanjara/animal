05a70cc4421a814ced47abb00116c89af5389def
vanjara <vanjara@outlook.com>
2020-09-12 19:26:43 -0700

    adding a constant struct and some commented code for next phase

--- animal_test.go
+++ animal_test.go
@@ -14,29 +14,6 @@ func TestNew(t *testing.T) {
 	}
 }
 
-func TestAskUserYesOrNo(t *testing.T) {
-	t.Parallel()
-	/*want := "yes"
-	got := animal.AskUserYesOrNo("Is it a horse")
-	if want != got {
-		t.Errorf("want %q, got %q", want, got)
-	}*/
-	testCases := []struct {
-		question string
-		want     string
-	}{
-		{question: "Is it a horse", want: "yes"},
-		{question: "Is it yes or no", want: "no"},
-		{question: "Does it have 2 legs", want: "no"},
-	}
-	for _, testCase := range testCases {
-		got := animal.AskUserYesOrNo(testCase.question)
-		if testCase.want != got {
-			t.Errorf("want %q, got %q", testCase.want, got)
-		}
-	}
-}
-
 func TestGetUserYesOrNo(t *testing.T) {
 	t.Parallel()
 	// multiple test cases
